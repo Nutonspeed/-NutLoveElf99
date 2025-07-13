795fc107baf505c9eaeefc5b13ad4f459ca55427
Nutonspeed <nuttapong161@gmail.com>
2025-07-13 14:28:10 +0700

    chore: add root eslint setup (#118)

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname })
+
+export default [
+  {
+    ignores: ['**'],
+  },
+  ...compat.extends('airbnb-base'),
+  ...compat.extends('plugin:react/recommended'),
+  ...compat.extends('plugin:react-hooks/recommended'),
+  ...compat.extends('plugin:vue/recommended'),
+  ...compat.extends('next'),
+  ...compat.extends('next/core-web-vitals'),
+  {
+    files: ['**/*.{js,jsx,ts,tsx,vue}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2023,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+]
