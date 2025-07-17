a3f3f006109c5525c2b67d79dcb0b10089936351
Nutonspeed <nuttapong161@gmail.com>
2025-07-18 01:51:27 +0700

    Remove obsolete 404 page

--- app/NotFoundClient.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { useSearchParams } from 'next/navigation'
-
-export default function NotFoundClient() {
-  const searchParams = useSearchParams()
-  const code = searchParams.get('code') ?? '404'
-  return <div>ไม่พบหน้านี้ (code: {code})</div>
-}
