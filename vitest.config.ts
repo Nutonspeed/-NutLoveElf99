e349a228a403f7705972f4e327b3db6e95ad0051
Nutonspeed <nuttapong161@gmail.com>
2025-07-18 11:43:22 +0700

    Remove chatwoot from vitest exclude (#461)

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    exclude: [...configDefaults.exclude, 'chatwoot/**'],
+    exclude: [...configDefaults.exclude],
   },
   resolve: {
     alias: {
